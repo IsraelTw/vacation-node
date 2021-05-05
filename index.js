e32402dc1d1a6736cc39c250f02799d45bfbc4f5
IsraelTw <azzzza19@gmail.com>
2021-05-05 21:50:14 +0300

    aa

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize('postgres://postgresql-sinuous-30280')
 // get users table
 const User = sequelize.define('user', {
     user_id: {
-        type: DataTypes.SERIAL,
+        type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true
     },
